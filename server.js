e579e0d1a88771ae078a5ee76ed1ce7019eb00df
sandip-saha <>
2022-08-20 22:29:49 +0530

    port change

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get('/sandip', (req, res) => {
     city: 'Howrah',
   })
 })
-const PORT = process.env.port || 8080
-console.log(process)
+const PORT = process.env.port || 80
+// console.log(process)
 app.listen(PORT, () => console.log(`app is running on port ${PORT}`))
